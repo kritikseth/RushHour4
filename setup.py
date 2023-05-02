c769b36a3aa5e8990164afac30559972249c82b8
Kritik Seth <39276404+kritikseth@users.noreply.github.com>
2023-05-02 03:43:51 -0400

    pypi

--- setup.py
+++ setup.py
@@ -2,19 +2,14 @@ import os
 import setuptools
 import RushHour4
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name = 'RushHour4',
-    version = varya.__version__,
-    author = varya.__author__,
+    version = RushHour4.__version__,
+    author = RushHour4.__author__,
     author_email = 'sethkritik@gmail.com',
     description = 'Reinforcement Learning Tools',
     py_modules = [''],
-    long_description = long_description,
     long_description_content_type = 'text/markdown',
-    url = varya.__url__,
     classifiers = [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
@@ -23,10 +18,20 @@ setuptools.setup(
     ],
     packages = setuptools.find_namespace_packages(include=['RushHour4', 'RushHour4.*']),
     install_requires = [
-    'pandas',
-    'numpy',
-    'torch',
-    'scikit-learn'
+        'contourpy==1.0.7',
+        'cycler==0.11.0',
+        'fonttools==4.39.3',
+        'kiwisolver==1.4.4',
+        'matplotlib==3.7.1',
+        'numpy==1.24.3',
+        'opencv-python==4.7.0.72',
+        'pandas==2.0.1',
+        'Pillow==9.5.0',
+        'pygame==2.3.0',
+        'pyparsing==3.0.9',
+        'pytz==2023.3',
+        'tqdm==4.65.0',
+        'tzdata==2023.3'
       ],
     python_requires='>=3.6'
 )
